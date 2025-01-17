acd84b4dba872812fa95b214ae0edf90c439149d
zarishbilal <zarishsuleman@gmail.com>
2025-01-17 16:32:32 -0700

    Made a week 2 page and added the link

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
 export default function Page() {
   return (
     <div>
       <h1>Hello World!</h1>
       <p>This is a simple web page</p>
+      <p>
+        Link to week 2: <Link href="week2">Week 2</Link>
+      </p>
     </div>
   );
 }
