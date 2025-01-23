af813d498b06510d54679d71bd0ade52b63d79d6
zarishbilal <zarishsuleman@gmail.com>
2025-01-22 18:02:03 -0700

    styled the heading

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,11 @@ import Item from "./item";
 import Itemlist from "./Itemlist";
 export default function Page() {
   return (
-    <main className="m-4 bg-gray-600 p-4">
-      <h1 className="text-2xl font-bold text-slate-200"> Shopping List</h1>
+    <main className=" bg-gray-600 p-4">
+      <h1 className="text-4xl m-4 ml-7 font-bold text-gray-300">
+        {" "}
+        Shopping List
+      </h1>
       <Itemlist />
     </main>
   );
